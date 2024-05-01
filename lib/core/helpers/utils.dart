9ff854d638d0d6f56a656915dd30cb17138c801b
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-05-01 20:56:58 +0300

    Add registerService function call in main
    Import utils.dart and call registerService function in main to initialize DioService.

--- lib/core/helpers/utils.dart
+++ lib/core/helpers/utils.dart
@@ -1,1 +1,6 @@
-// Future<void> registerService()
+import 'package:get/get.dart';
+import 'package:live_assesment/core/networking/dio_service.dart';
+
+Future<void> registerService() async {
+  Get.put(DioService());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:live_assesment/core/helpers/utils.dart';
 import 'package:live_assesment/live_app.dart';
 
-void main() {
+void main() async {
+  await registerService();
   runApp(LiveApp());
 }
