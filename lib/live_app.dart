dff779c37cf0a5a48594f67f0ca1710269b9fb1c
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-05-01 20:30:02 +0300

    Add LiveApp widget and update main.dart to use LiveApp
    The LiveApp widget is added to the project, and the main.dart file is updated
    to use the LiveApp widget instead of the MyApp widget. This change ensures
    that the LiveApp widget is the root widget of the application.

---
+++ lib/live_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LiveApp extends StatelessWidget {
+  const LiveApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:live_assesment/main.dart';
+import 'package:live_assesment/live_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LiveApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
